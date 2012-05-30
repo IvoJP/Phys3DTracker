fb52f2675866b4a070d7d553cc72463d2436392b
Andrew Bianco <biancoa32@gmail.com>
2012-05-29 19:23:58 -0700

    now as function definition

--- DataIn.py
+++ DataIn.py
@@ -32,27 +32,29 @@ import NiffteGeo as ngeo
 import Voxel as voxel
 import math
 
-fil = open('NIFFTE-alphas.dat')
-lines = fil.readlines()
-ind = 0
-event = 0
-events = []
-for line in lines:
-	ind+=1
-	s=line.split()
-	if len(s) > 5:
-		eventPoints = []
-		event = s[2]
-		count = s[5]
-		for i in range(int(count)):
-			eventLines=lines[ind+i]
-			eventData=eventLines.split()
-			voxel.volume=int(eventData[0])
-			voxel.row=int(eventData[1])
-			voxel.column=int(eventData[2])
-			voxel.bucket=int(eventData[3])
-			voxel.adc=int(eventData[4])
-			xyz = ngeo.MapVoxeltoXYZ(voxel)
-			eventPoints.append(xyz)
-		events.append(eventPoints)
-			
+def dataReader():
+	fil = open('NIFFTE-alphas.dat')
+	lines = fil.readlines()
+	ind = 0
+	event = 0
+	events = []
+	for line in lines:
+		ind+=1
+		s=line.split()
+		if len(s) > 5:
+			eventPoints = []
+			event = s[2]
+			count = s[5]
+			for i in range(int(count)):
+				eventLines=lines[ind+i]
+				eventData=eventLines.split()
+				voxel.volume=int(eventData[0])
+				voxel.row=int(eventData[1])
+				voxel.column=int(eventData[2])
+				voxel.bucket=int(eventData[3])
+				voxel.adc=int(eventData[4])
+				xyz = ngeo.MapVoxeltoXYZ(voxel)
+				eventPoints.append(xyz)
+			events.append(eventPoints)
+	return events
+		
